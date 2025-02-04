67cbdbcac7b958a3c35015064498c6d98347222f
Trey Brown <treybrown3293@gmail.com>
2025-02-04 11:09:38 -0500

    Update jekins-user-data.sh

--- jekins-user-data.sh
+++ jekins-user-data.sh
@@ -7,7 +7,7 @@ sudo apt upgrade -y
 sudo apt install -y fontconfig openjdk-17-jre
 
 # Add Jenkins repository and key
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key |
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key 
  sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
  echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" 
  sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
