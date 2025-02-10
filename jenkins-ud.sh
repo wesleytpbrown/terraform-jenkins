4667733026a403cc3a6666709ddf66461592c57c
Trey Brown <treybrown3293@gmail.com>
2025-02-10 16:19:37 -0500

    Update jenkins-ud.sh

--- jenkins-ud.sh
+++ jenkins-ud.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Update the package list
-#sudo apt update -y
-#sudo apt upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 
 # Install Java (required for Jenkins)
-#sudo apt install -y fontconfig openjdk-17-jre
+sudo apt install -y fontconfig openjdk-17-jre
 
 # Add Jenkins repository and key
-#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key 
-#sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-#echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" 
-#sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key 
+sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" 
+sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # Update package list again and install Jenkins
-#sudo apt update -y
-#sudo apt install -y jenkins
+sudo apt update -y
+sudo apt install -y jenkins
 
-# Start and enable Jenkins service
-#sudo systemctl enable jenkins
-#sudo systemctl start jenkins
+#Start and enable Jenkins service
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
