691fb5f2b537e9c0e2b0aab2c733917f622ccd50
Trey Brown <treybrown3293@gmail.com>
2025-02-10 22:54:20 -0500

    Update jenkins-ud.sh

--- jenkins-ud.sh
+++ jenkins-ud.sh
@@ -7,10 +7,9 @@ sudo apt upgrade -y
 sudo apt install -y fontconfig openjdk-17-jre
 
 # Add Jenkins repository and key
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key 
-sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" 
-sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 
 # Update package list again and install Jenkins
 sudo apt update -y
